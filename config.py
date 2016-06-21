a7aa2f5385782c50b7c430816905707c577cd347
Mustafa Arici <mustafa.arici@neu.edu.tr>
2016-06-21 19:42:49 +0300

    refactor(config): relocate config into seperate module

---
+++ config.py
@@ -0,0 +1,39 @@
+from inlet.udp import MulticastUDPInlet
+from inlet.pcap import PCAPInlet
+from inlet.websocket import WebSocketInlet
+from outlet.stdout import STDOUTOutlet
+from outlet.websocket import WebSocketOutlet
+from middleware.json import JSONDecodeMiddleware, JSONEncodeMiddleware
+from middleware.canethernet import CANEthernetMiddleware
+
+CONFIG = {
+    'inlets': [
+        (MulticastUDPInlet, {
+            'host': '239.255.60.60',
+            'port': 4876,
+            'middlewares': [(CANEthernetMiddleware, {})]
+        }),
+        (WebSocketInlet, {
+            'port': 9001,
+            'middlewares': [(JSONDecodeMiddleware, {})]
+        }),
+        # (PCAPInlet, {
+        #     'filepath': 'data/sample.pcap',
+        #     'postproccessors': [(CANEthernetMiddleware, {})]
+        # })
+    ],
+    'outlets': [
+        (WebSocketOutlet, {
+            'port': 9000,
+            'middlewares': [(JSONEncodeMiddleware, {})]
+        }),
+        (STDOUTOutlet, {
+            'middlewares': []
+        }),
+
+    ],
+    'middlewares': [
+        # (CANEthernetMiddleware, {})
+    ],
+
+}
--- logger.py
+++ logger.py
@@ -52,45 +52,7 @@ class BusLogger():
 
 if __name__ == '__main__':
     import sys
-    from inlet.udp import MulticastUDPInlet
-    from inlet.pcap import PCAPInlet
-    from inlet.websocket import WebSocketInlet
-    from outlet.stdout import STDOUTOutlet
-    from outlet.websocket import WebSocketOutlet
-    from middleware.json import JSONDecodeMiddleware, JSONEncodeMiddleware
-    from middleware.canethernet import CANEthernetMiddleware
-
-    config = {
-        'inlets': [
-            (MulticastUDPInlet, {
-                'host': '239.255.60.60',
-                'port': 4876,
-                'middlewares': [(CANEthernetMiddleware, {})]
-            }),
-            (WebSocketInlet, {
-                'port': 9001,
-                'middlewares': [(JSONDecodeMiddleware, {})]
-            }),
-            # (PCAPInlet, {
-            #     'filepath': 'data/sample.pcap',
-            #     'postproccessors': [(CANEthernetMiddleware, {})]
-            # })
-        ],
-        'outlets': [
-            (WebSocketOutlet, {
-                'port': 9000,
-                'middlewares': [(JSONEncodeMiddleware, {})]
-            }),
-            (STDOUTOutlet, {
-                'middlewares': []
-            }),
-
-        ],
-        'middlewares': [
-            # (CANEthernetMiddleware, {})
-        ],
-
-    }
-    logger = BusLogger(config)
+    from config import CONFIG
+    logger = BusLogger(CONFIG)
     log.startLogging(sys.stdout)
     logger.run()
