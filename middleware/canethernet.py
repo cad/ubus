eb8bb3b77c51bf7d571a414712f62749e821772d
Mustafa Arici <mustafa.arici@neu.edu.tr>
2016-06-21 20:08:14 +0300

    refactor(middleware): inject only data payload

--- middleware/canethernet.py
+++ middleware/canethernet.py
@@ -5,7 +5,8 @@ from protocol import Ra27CANProtocolParser
 class CANEthernetMiddleware(BaseMiddleware):
     def apply(self, message):
         try:
-            message = Ra27CANProtocolParser(message).parse()
+            message = Ra27CANProtocolParser(message).parse().data
         except Exception as e:
             print e
+
         return message
